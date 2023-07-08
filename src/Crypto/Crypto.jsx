6fc4d10f469ce6d119ad776b57e7acfe1639f2aa
Prathmesh Gokhe <pkgokhe1969@gmail.com>
2023-07-08 17:54:03 +0530

    frontend done

--- src/Crypto/Crypto.jsx
+++ src/Crypto/Crypto.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 import "./crypto.scss"
 
 const Crypto = () => {
+
+    const [crypto, setcrypto] = useState(null);
+
+    useEffect(() => {
+
+        const fetchApi = async () => {
+            const url = `https://api.themoviedb.org/3/search/movie?api_key=b833cf42a61d4d5b116221599edd127a&query=${search}`
+            const response = await fetch(url)
+            const resJson = await response.json();
+            // console.log(resJson)
+            setMovie(resJson)
+        }
+
+        fetchApi();
+    }, [search])
+
     return (
         <>
             <div className='container'>
