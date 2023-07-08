47c9870c714d29625c6097dbd8b686707ca90259
Prathmesh Gokhe <pkgokhe1969@gmail.com>
2023-07-08 15:53:44 +0530

    first commit

---
+++ src/Crypto/Crypto.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./crypto.scss"
+
+const Crypto = () => {
+    return (
+        <>
+            <div className='container'>
+                <div className='main'>
+                    <div className='heading'>
+                        <h1>Realcoin</h1>
+                    </div>
+                    <hr />
+                    <div className='crypto-container'>
+                        <div className='crypto-info'>
+                            <div className='image'>
+                            </div>
+                            <div className='info'>
+                                <h1>Bitcoin</h1>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Crypto
